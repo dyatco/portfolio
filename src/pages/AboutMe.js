2344d1b0eb862c4f2ff38dfb2d696b3d4e21d755
Daphne Yatco <dyatco@gmail.com>
2023-01-02 23:09:13 +0800

    edited project photos

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,42 +1,47 @@
+import profile from '../Images/profile.png'
+
 export default function AboutMe(){
   return(
-    <div class="min-vh-100 justify-content-center text-center">
+    <div class="min-vh-100 justify-content-center text-start">
       <div class="row min-vh-100">
-       <div class="mb-0 col-12  justify-content-center">
-        <h1>About Me</h1>
-        <div class="min-vw-100 row d-flex justify-content-center">
-        <p class="w-75 pb-5">   
-          In August 2022, I took an intensive bootcamp in web development and found myself enjoying the front-end side of coding. I work using with HTML, CSS, and JavaScript.
+       <div class="mb-0 col-6 p-0">
+        
+        <img src={profile} alt="profile" className="img-fluid w-75 d-flex justify-content-start ms-5"/>
+        </div>
+        <div class="col-6 p-0">
+        <h1 class="mb-0 me-5">Developer/Explorer</h1>
+       
+          <p class="pb-5 me-5">   
+            Hi, I'm Daphne. In August 2022, I took an intensive bootcamp in web development and found myself enjoying the front-end side of coding. I work using with HTML, CSS, and JavaScript.<br/><br/>
 
-          Apart from being a web developer, I am also an explorer. I love discovering places and trying new things.
-        </p>
-        <h1>Get in Touch</h1>
-        <p class="w-75 pb-5">
-         I am currently looking for new opportunities. <br/>
-         My inbox is always open for any comments or questions!
-        </p>
+            Apart from being a web developer, I am also an explorer. I love discovering places and trying new things.
+          </p>
+          <h1>Get in Touch</h1>
+          <p class="w-75">
+           I am currently looking for new opportunities. <br/>
+           My inbox is always open for any comments or questions!
+          </p>
 
-         <button type="button" class="w-100 mt-2" data-toggle="modal" data-target="#exampleModal">Send email</button>
+           <button type="button" class="btn btn-warning w-50 mt-2" data-toggle="modal" data-target="#exampleModal">Send email</button>
 
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content text-dark">
-                  <div class="modal-header">
-                    <h5 class="modal-title text-dark" id="exampleModalLabel">Thank you for your email!</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content text-dark">
+                    <div class="modal-header">
+                      <h5 class="modal-title text-dark" id="exampleModalLabel">Thank you for your email!</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
             
         </div>
         </div>
       </div>
-    </div>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,8 @@
 import { Fade } from 'react-reveal'
 import TileCalc from '../Images/TileCalc.png'
 import SpecSheet from '../Images/SpecSheet.png'
+import Tile from '../Images/tile.png'
+import Spec from '../Images/spec.png'
 
 export default function Projects(){
   return(
@@ -8,40 +10,41 @@ export default function Projects(){
   <div class="container-fluid p-0 min-vh-75 min-vw-100 d-flex justify-content-center text-center pb-5">
       <div class="row">
        <div class="mb-0 col-12  justify-content-center">
-        <h1>Projects</h1>
         
         {/*Project 1*/}
-        <div class="row min-vw-100 justify-content-center m-0 p-0 pt-3 container-fluid h-100">
-          <div class="mb-3 h-100 d-sm-flex">
-            <div class="col-12 col-sm-8 px-0 pb-sm-5 mb-sm-5 d-sm-flex justify-content-end">
-              <img src={TileCalc} alt="tilecalc" className="projectPhoto w-75 img-fluid"/>
+        <div class="row min-vw-100 justify-content-center m-0 p-0 pt-3 h-100">
+          <h1>TILE CALCULATOR</h1>
+          <div class="mb-3">
+
+            <div class="col-12 px-0 d-flex justify-content-center">
+              <img src={Tile} alt="tilecalc" className="projectPhoto w-75 img-fluid"/>
             </div>
-          
-            <div class="col-sm-4 text-center text-sm-start p-0 ps-sm-3 d-flex justify-content-center justify-content-sm-start">
-               <p class="projectText w-50 mb-0 mt-3 mt-sm-0">TILE CALCULATOR<br/>
-               Computes how many tiles are needed to fill a floor area<br/>
-               html css bootstrap
-               </p>
+          </div>
+
+           <div class="col-12 text-center p-0 d-flex justify-content-center">
+             <p class="projectDesc">
+             Computes how many tiles are needed to fill a floor area
+             <p class="projectTools pt-1">html css bootstrap</p>
+             </p>
             </div>
+
+          <h1 class="pt-5">SPECIFICATIONS SHEETS</h1>
+
+          <div class="col-12 px-0 d-flex justify-content-center">
+            <img src={Spec} alt="SpecSheet" className="projectPhoto w-75 img-fluid"/>
+          </div>
+
+          <div class="col-12 text-center p-0 d-flex justify-content-center">
+             <p class="projectDesc pt-3">
+               Digital tool for recording for interior design projects
+              <p class="projectTools pt-1">html css bootstrap</p>
+             </p>
           </div>
         </div>
         {/*Project 2*/}
-
+        
+          </div>
+          </div>
       </div>
-      </div>
-  </div>
   )
 }
-
- 
-          {/*
-           {/*<p class ="w-50 ">SPECIFICATIONS SHEETS<br/>
-           Digital tool for recording for interior design projects<br/>
-           html css bootstrap
-           </p>*/}
-          {/*</div>
-      
-          <div class="col-8 h-100">
-            <img src={SpecSheet} alt="SpecSheet" class="projectPhoto2 w-75"/>
-          </div>
-          </div>*/}
