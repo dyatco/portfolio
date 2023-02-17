f384c89d0ce34b48d4e625770e8d299b3a5fb153
Daphne Yatco <dyatco@gmail.com>
2023-02-18 00:37:10 +0800

    fixed navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,28 @@
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import { Fade } from 'react-reveal'
+import { useState } from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Fade } from 'react-reveal';
+import { HashLink } from 'react-router-hash-link';
+import { Home } from '../pages/Home';
+import { Link } from "react-router-dom";
 
-  export default function NavBar(){
+export default function NavBar(){
   
+const [expanded, setExpanded] = useState(false);
+
   return(
-    <div class="boxNav min-vw-100 px-5">     
-      <Navbar bg="none" expand="lg" variant="dark">
-        <Container>
+    <div class="container-fluid boxNav px-0">     
+      <Navbar bg="none" expand="lg" variant="dark" expanded={expanded} className="navContainer">
           <Navbar.Brand href="#home" class="ms-3 nav">DY</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <HashLink className="navItem" to="/#home" onClick={() => setExpanded(false)}>Home</HashLink>
+              <HashLink className="navItem" to="/#skills" onClick={() => setExpanded(false)}>Skills</HashLink>
+              <HashLink className="navItem" to="/#projects" onClick={() => setExpanded(false)}>Projects</HashLink>
+              <HashLink className="navItem" to="/#about" onClick={() => setExpanded(false)}>About</HashLink>
+              <HashLink className="navItem" to="/#contact" onClick={() => setExpanded(false)}>Contact</HashLink>
             </Nav>
           </Navbar.Collapse>
-        </Container>
       </Navbar>
     </div>
       )
